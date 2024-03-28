ffd64041120f0156a88aa43fa50a14e78d9ed969
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-28 17:20:44 +0100

    add variable activeIndexSlides in script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 import {slides} from './data.js';
+const {createApp} = Vue;
+createApp({
+    data(){
+        return {
+            slides: slides,
+            activeIndexSlide:0,
+
+        }
+    }
+}).mount('#app');
